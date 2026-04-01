c94abaa9886651fd505319d1f156535ddb144299
Theresia Calista <theresiacalista57@gmail.com>
2026-04-01 23:59:49 +0700

    set up docker

--- app/scripts/load_to_db.py
+++ app/scripts/load_to_db.py
@@ -7,11 +7,12 @@ def load_to_db(df):
     load dataframe to postgresql database titled choco_db
     """
     load_dotenv()
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
-    DB_HOST = os.getenv('DB_HOST')
-    DB_USER = os.getenv('DB_USER')
-    DB_PORT = os.getenv('DB_PORT')
-    engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/choco_db")
+    POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+    POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+    POSTGRES_USER = os.getenv('POSTGRES_USER')
+    POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+    POSTGRES_DB = os.getenv('POSTGRES_DB')
+    engine = create_engine(f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}")
 
     print("Loading DataFrame to database...")
     df.to_sql("choco_stats", engine, if_exists="replace", index=False)
