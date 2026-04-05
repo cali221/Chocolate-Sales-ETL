cd73b1c293adc53b64c52d7ea5e87a7ddf2e2a93
cali221 <theresiacalista57@gmail.com>
2026-04-05 16:59:07 +0700

    added some dbt singular tests

---
+++ transform/choco_sales/tests/dim_sales_people_has_all_staging_sales_people.sql
@@ -0,0 +1,7 @@
+-- depends_on: {{ ref('stg_choco_db__choco_stats') }}
+
+-- check if there is a sales person in the staging table derived from kaggle 
+-- historical data that's not present in the sales people dimension table
+SELECT DISTINCT sales_person 
+FROM {{ ref('stg_choco_db__choco_stats') }}
+WHERE sales_person NOT IN (SELECT name FROM {{ ref('dim_sales_people') }})
