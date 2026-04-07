05b139a628b9fb6ea20d4c3030106d085727569b
cali221 <theresiacalista57@gmail.com>
2026-04-07 15:19:41 +0700

    progress with dummy api, added more endpoints

--- dummy_api/api.py
+++ dummy_api/api.py
@@ -1,10 +1,21 @@
 from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException, Query
 from contextlib import asynccontextmanager
-from sqlmodel import Session
-from .db_models import Product, Country, Customer, Status, Order, StatusHistory, OrderProduct
+from sqlmodel import Session, select
 from .database import setup_db, get_engine
-from .api_models import ProductPublic, ProductCreate
+from .db_models import (Product, 
+                        Country, 
+                        Customer, 
+                        Status, 
+                        Order,
+                        OrderItem,
+                        StatusHistory)
+from .api_models import (ProductPublic, ProductCreate, 
+                         CountryCreate, CountryPublic, 
+                         CustomerPublic, CustomerCreate, 
+                         StatusPublic, StatusCreate,
+                         OrderCreate, OrderPublic,
+                         OrderItemCreate, OrderItemPublic)
 
 engine = get_engine()
 
@@ -23,10 +34,115 @@ async def lifespan(app: FastAPI):
     
 app = FastAPI(lifespan=lifespan)
 
+# endpoint to create a product
 @app.post("/products/", response_model=ProductPublic)
-def create_product(product: ProductCreate, session: SessionDep) -> Product:
+def create_product(product: ProductCreate, session: SessionDep):
     db_product = Product.model_validate(product)
     session.add(db_product)
     session.commit()
     session.refresh(db_product)
     return db_product
+
+# endpoint to create a country
+@app.post("/countries/", response_model=CountryPublic)
+def create_country(country: CountryCreate, session: SessionDep):
+    db_country = Country.model_validate(country)
+    session.add(db_country)
+    session.commit()
+    session.refresh(db_country)
+    return db_country
+
+# endpoint to create customer
+@app.post("/customers/", response_model=CustomerPublic)
+def create_customer(customer: CustomerCreate, session: SessionDep):
+    db_customer = Customer.model_validate(customer)
+
+    # get country based on the country_id input
+    country = session.get(Country, db_customer.country_id)
+
+    # if no country was found, show error
+    if not country:
+        raise HTTPException(status_code=404, detail="Country not found")
+    else:
+        # get the country's name
+        country_name = country.name
+
+        session.add(db_customer)
+        session.commit()
+        session.refresh(db_customer)
+
+        # return the customers table fields + the country's name
+        return CustomerPublic(
+            id=db_customer.id,
+            name=db_customer.name,
+            email=db_customer.email,
+            username=db_customer.username,
+            country_name=country_name,
+            country_id=db_customer.country_id
+        )
+
+# endpoint to create a status
+@app.post("/status/", response_model=StatusPublic)
+def create_status(status: StatusCreate, session: SessionDep):
+    db_status = Status.model_validate(status)
+    session.add(db_status)
+    session.commit()
+    session.refresh(db_status)
+    return db_status
+
+# endpoint to create an order
+@app.post("/orders/", response_model=OrderPublic)
+def create_order(order: OrderCreate, session: SessionDep):
+    get_status_statement = select(Status).where(Status.name == "Pending")
+    status_results = session.exec(get_status_statement)
+    status = status_results.first()
+
+    if not status:
+        raise HTTPException(status_code=404, detail="Pending order status not found")
+    else:
+        try:
+            order_data = Order(
+                current_status_id = status.id,
+                customer_id = order.customer_id,
+                total_amount = order.total_amount
+            )
+
+            session.add(order_data)
+            session.flush()
+            session.refresh(order_data)
+
+            print(f"ORDER DATA: {order_data}")    
+
+            for item in order.items:
+                product = session.get(Product, item.product_id)
+                
+                if not product:
+                    raise HTTPException(status_code=404, detail="Country not found")
+                else:
+                    item_to_add = OrderItem(
+                        product_id = product.id,
+                        order_id = order_data.id,
+                        quantity = item.quantity,
+                        price_at_purchase = product.current_price
+                    )
+                    session.add(item_to_add)
+                    session.flush()
+                    session.refresh(item_to_add)
+                    print(f"ITEM TO ADD: {item_to_add}")
+
+            status_history_to_add = StatusHistory( 
+                order_id = order_data.id,
+                status_id = status.id
+            )
+
+            session.add(status_history_to_add)
+            session.flush()
+            session.refresh(status_history_to_add)
+            print(f"STATUS HISTORY TO ADD:{status_history_to_add}")
+
+            session.commit()
+
+            return order_data
+        except:
+            session.rollback()
+            raise
--- dummy_api/api_models.py
+++ dummy_api/api_models.py
@@ -1,12 +1,84 @@
 from sqlmodel import Field, SQLModel, String
 from decimal import Decimal
-from pydantic import Field
-from .db_models import DimensionsBase
+from pydantic import Field, field_validator
+from datetime import datetime
 
-class ProductPublic(DimensionsBase):
+# products table api models
+class ProductPublic(SQLModel):
     id: int
+    name: str
     current_price: Decimal 
 
-class ProductCreate(DimensionsBase):
-    name: str = Field(max_length=100)
+class ProductCreate(SQLModel):
+    name: str = Field(max_length=50)
     current_price: Decimal = Field(max_digits=10, decimal_places=3)
+
+# countries table api models
+class CountryPublic(SQLModel):
+    id: int
+    name: str
+
+class CountryCreate(SQLModel):
+    name: str = Field(max_length=50)
+
+# customers table api models
+class CustomerPublic(SQLModel):
+    id: int
+    username: str 
+    email: str
+    name: str
+    country_id: int
+    country_name: str
+
+class CustomerCreate(SQLModel):
+    name: str = Field(max_length=50)
+    username: str = Field(max_length=50)
+    email: str = Field(max_length=100)
+    country_id: int
+
+# status table api models
+class StatusPublic(SQLModel):
+    id: int
+    name: str
+
+class StatusCreate(SQLModel):
+    name: str = Field(max_length=50)
+
+# order items api models
+class OrderItemPublic(SQLModel):
+    id: int
+    product_id: int
+    order_id: int
+    quantity: int
+    price_at_purchase: Decimal
+
+class OrderItemCreate(SQLModel):
+    quantity: int
+    product_id: int
+
+# orders table api models
+class OrderPublic(SQLModel):
+    id: int
+    total_amount: Decimal
+    created_at: datetime
+    last_updated_at: datetime
+    current_status_id: int
+    customer_id: int  
+
+class OrderCreate(SQLModel):
+    total_amount: Decimal
+    customer_id: int
+    items: list[OrderItemCreate]  
+
+    # check if there are duplicated product IDs in the list of items purchased
+    # throw error if they exist
+    @field_validator('items', mode='after')  
+    @classmethod
+    def no_duplicate_product_id(cls, items: list):
+       product_ids = [item.dict()['product_id'] for item in items]
+
+       if len(product_ids) != len(set(product_ids)):
+        raise ValueError("Unexpected behavior detected: duplicated product IDs in one order")
+
+       return items
+    
