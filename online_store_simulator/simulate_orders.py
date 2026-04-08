f8acb25ab240ff39ec872b07915fbd5db3319dde
cali221 <theresiacalista57@gmail.com>
2026-04-09 02:47:01 +0700

    some progress with implementing online orders simulator

--- online_store_simulator/simulate_orders.py
+++ online_store_simulator/simulate_orders.py
@@ -1,1 +1,22 @@
-#r = requests.post('https://httpbin.org/post', data={'key': 'value'})
+import requests
+
+order_data = {
+  "customer_id": 1,
+  "items": [
+    {
+      "quantity": 2,
+      "product_id": 2
+    },
+    {
+      "quantity": 2,
+      "product_id": 1
+    }
+  ],
+  "tax_amount": 3,
+  "discount_amount": 3,
+  "shipping_costs_amount": 3
+}
+
+r = requests.post('http://localhost:8000/orders', json=order_data)
+
+print(r.json())
