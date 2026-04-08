c9f3f68fb86da5260a5fcca12d6526ec6e5f8b08
cali221 <theresiacalista57@gmail.com>
2026-04-08 21:01:56 +0700

    finished dummy api's post endpoints

--- dummy_api/database.py
+++ dummy_api/database.py
@@ -16,14 +16,27 @@ def get_engine():
     POSTGRES_PORT = os.getenv('POSTGRES_PORT')
     POSTGRES_DB = os.getenv('POSTGRES_DB')
         
-    engine = create_engine(f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}")
+    engine = create_engine(f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}", connect_args={"options": "-c timezone=utc"})
     return engine
 
 def setup_db(engine):
-    print('setup_db CALLED')
+    print("setup_db called, setting up OLTP database:")
+
+    # create the schema
+    print("Create oltp_online_store schema if it doesn't exist")
     with engine.connect() as connection:
         connection.execute(text("CREATE SCHEMA IF NOT EXISTS oltp_online_store"))
         connection.commit()
 
-    print(SQLModel.metadata.tables.keys())
+    # add the tables
+    print("Adding the tables")
     SQLModel.metadata.create_all(engine)
+
+    # add db triggers
+    print("Adding database triggers")
+    with engine.connect() as connection:
+        with open("setup_db_triggers.psql") as file:
+            query = text(file.read())
+            connection.execute(query)
+            connection.commit()
+
