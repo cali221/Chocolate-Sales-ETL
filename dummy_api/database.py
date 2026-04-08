a37d856fa3101a87e0d3d5d367144ff6c067902f
cali221 <theresiacalista57@gmail.com>
2026-04-08 21:35:36 +0700

    modified setup_db function so it calls the scripts to fill the DB

--- dummy_api/database.py
+++ dummy_api/database.py
@@ -19,24 +19,44 @@ def get_engine():
     engine = create_engine(f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}", connect_args={"options": "-c timezone=utc"})
     return engine
 
+def execute_psql_script(path_to_script, conn):
+    with open(path_to_script) as file:
+        query = text(file.read())
+        conn.execute(query)
+        conn.commit()
+
 def setup_db(engine):
     print("setup_db called, setting up OLTP database:")
 
     # create the schema
-    print("Create oltp_online_store schema if it doesn't exist")
+    print("Create oltp_online_store schema if it doesn't exist...")
     with engine.connect() as connection:
         connection.execute(text("CREATE SCHEMA IF NOT EXISTS oltp_online_store"))
         connection.commit()
 
     # add the tables
-    print("Adding the tables")
+    print("Adding the tables...")
     SQLModel.metadata.create_all(engine)
 
-    # add db triggers
-    print("Adding database triggers")
     with engine.connect() as connection:
-        with open("setup_db_triggers.psql") as file:
-            query = text(file.read())
-            connection.execute(query)
-            connection.commit()
+        sql_scripts_dir = Path(__file__).parent / "sql_scripts"
+
+        # add db triggers
+        print("Adding database triggers...")
+        execute_psql_script(sql_scripts_dir / "setup_db_triggers.psql", connection)
+
+        # pre-fill the DB
+        print("Pre-filling the database")
+
+        # fill countries table
+        execute_psql_script(sql_scripts_dir / "prefill_oltp_countries.psql", connection)
+
+        # fill customers table
+        execute_psql_script(sql_scripts_dir / "prefill_oltp_customers.psql", connection)
+
+        # fill products table
+        execute_psql_script(sql_scripts_dir / "prefill_oltp_products.psql", connection)
+
+        # fill status table
+        execute_psql_script(sql_scripts_dir / "prefill_oltp_status.psql", connection)
 
