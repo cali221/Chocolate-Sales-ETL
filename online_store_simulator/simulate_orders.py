8830d2904e9b06fadc3f7870a4aca5ccfd3e53ee
cali221 <theresiacalista57@gmail.com>
2026-04-09 04:06:49 +0700

    added a script to simulate a random order

--- online_store_simulator/simulate_orders.py
+++ online_store_simulator/simulate_orders.py
@@ -1,23 +1,50 @@
+# python3 simulate_orders.py
 import requests
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+import random
 
+if not os.getenv('USING_DOCKER'):
+    print('Not using docker, loading .env.local')
+    dotenv_path = Path(__file__).parent.parent / '.env.local'
+    load_dotenv(dotenv_path)
+
+host = os.getenv('POSTGRES_HOST')
+print(f"host: {host}")
+
+# get product IDs (limit 100)
+products = requests.get(f"http://{host}:8000/products").json()
+product_ids = [product['id'] for product in products]
+
+# get customer IDs (limit 100)
+customers = requests.get(f"http://{host}:8000/customers").json()
+customer_ids = [customer['id'] for customer in customers]
+
+# print the available IDs for products and customers
+print(f"Product IDs: {product_ids}")
+print(f"Customer IDs: {customer_ids}")
+
+# get a random number of distinct products to order
+number_of_items = random.randint(1, 10)
+
+# get number_of_items unique product IDs 
+selected_product_ids = random.sample(product_ids, number_of_items)
+
+# get a list of the items to order with their quantities
+order_items = [ {"quantity": random.randint(1, 500), "product_id": item} for item in selected_product_ids]
+
+# set up the order data
 order_data = {
-  "customer_id": 1,
-  "items": [
-    {
-      "quantity": 2,
-      "product_id": 2
-    },
-    {
-      "quantity": 2,
-      "product_id": 1
-    }
-  ],
-  "tax_amount": 3,
-  "discount_amount": 3,
-  "shipping_costs_amount": 3
+  "customer_id": random.choice(customer_ids),
+  "items": order_items,
+  "tax_amount": round(random.uniform(1, 15), 3),
+  "discount_amount": round(random.uniform(1, 10), 3),
+  "shipping_costs_amount": round(random.uniform(1, 15), 3)
 }
 
-# TODO: change localhost to dummy_api based on whether or not docker is used
-r = requests.post('http://localhost:8000/orders', json=order_data)
+# create an order
+order_created = requests.post(f"http://{host}:8000/orders", json=order_data)
 
-print(r.json())
+# print the json results of the endpoint
+print(order_created.json())
