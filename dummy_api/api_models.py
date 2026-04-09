d8eb4dadce1dab4cc90b145f7bdc78d92f0345e9
cali221 <theresiacalista57@gmail.com>
2026-04-09 15:16:51 +0700

    added more pydantic checks for api

--- dummy_api/api_models.py
+++ dummy_api/api_models.py
@@ -5,78 +5,78 @@ from datetime import datetime
 
 # products table api models
 class ProductPublic(SQLModel):
-    id: int
-    name: str
-    current_price: Decimal = Field(max_digits=10, decimal_places=3)
+    id: int = Field(nullable=False)
+    name: str = Field(min_length=1, nullable=False, max_length=50)
+    current_price: Decimal = Field(max_digits=10, decimal_places=3, ge=0, nullable=False)
 
 class ProductCreate(SQLModel):
-    name: str = Field(max_length=50)
-    current_price: Decimal = Field(max_digits=10, decimal_places=3)
+    name: str = Field(max_length=50, min_length=1, nullable=False)
+    current_price: Decimal = Field(max_digits=10, decimal_places=3, ge=0)
 
 # countries table api models
 class CountryPublic(SQLModel):
-    id: int
-    name: str
+    id: int = Field(nullable=False)
+    name: str = Field(nullable=False, max_length=50, min_length=2)
 
 class CountryCreate(SQLModel):
-    name: str = Field(max_length=50)
+    name: str = Field(max_length=50, min_length=2)
 
 # customers table api models
 class CustomerPublic(SQLModel):
-    id: int
-    username: str 
-    email: str
-    name: str
-    country_id: int
-    country_name: str
+    id: int = Field(nullable=False)
+    username: str = Field(max_length=50, min_length=1, nullable=False)
+    email: str = Field(max_length=100, min_length=2, nullable=False)
+    name: str = Field(max_length=50, min_length=1, nullable=False)
+    country_id: int = Field(nullable=False)
+    country_name: str = Field(nullable=False)
 
 class CustomerCreate(SQLModel):
-    name: str = Field(max_length=50)
-    username: str = Field(max_length=50)
-    email: str = Field(max_length=100)
-    country_id: int
+    name: str = Field(max_length=50, min_length=1, nullable=False)
+    username: str = Field(max_length=50, min_length=1, nullable=False)
+    email: str = Field(max_length=100, min_length=2, nullable=False)
+    country_id: int = Field(nullable=False)
 
 # status table api models
 class StatusPublic(SQLModel):
-    id: int
-    name: str
+    id: int = Field(nullable=False)
+    name: str = Field(max_length=50, min_length=1, nullable=False)
 
 class StatusCreate(SQLModel):
-    name: str = Field(max_length=50)
+    name: str = Field(max_length=50, min_length=1, nullable=False)
 
 # order items api models
 class OrderItemPublic(SQLModel):
-    id: int
-    product_id: int
-    product_name: str
-    order_id: int
-    quantity: int
-    price_per_unit_at_purchase: Decimal = Field(max_digits=10, decimal_places=3)
+    id: int = Field(nullable=False)
+    product_id: int = Field(nullable=False)
+    product_name: str = Field(max_length=50, min_length=1, nullable=False)
+    order_id: int = Field(nullable=False)
+    quantity: int = Field(nullable=False, ge=1)
+    price_per_unit_at_purchase: Decimal = Field(max_digits=10, decimal_places=3, ge=0, nullable=False)
 
 class OrderItemCreate(SQLModel):
-    quantity: int
-    product_id: int
+    quantity: int = Field(nullable=False, ge=0)
+    product_id: int = Field(nullable=False)
 
 # orders table api models
 class OrderPublic(SQLModel):
-    id: int
-    created_at: datetime
-    status_last_updated_at: datetime
-    current_status_id: int
-    current_status_name: str
-    customer_id: int 
-    customer_name: str
-    tax_amount: Decimal = Field(max_digits=10, decimal_places=3)
-    discount_amount: Decimal = Field(max_digits=10, decimal_places=3)
-    shipping_costs_amount: Decimal = Field(max_digits=10, decimal_places=3)
+    id: int = Field(nullable=False)
+    created_at: datetime = Field(nullable=False)
+    status_last_updated_at: datetime = Field(nullable=False)
+    current_status_id: int = Field(nullable=False)
+    current_status_name: str = Field(max_length=50, min_length=1, nullable=False)
+    customer_id: int = Field(nullable=False)
+    customer_name: str = Field(max_length=50, min_length=1, nullable=False)
+    tax_amount: Decimal = Field(max_digits=10, decimal_places=3, ge=0)
+    discount_amount: Decimal = Field(max_digits=10, decimal_places=3, ge=0)
+    shipping_costs_amount: Decimal = Field(max_digits=10, decimal_places=3, ge=0)
     order_items: list[OrderItemPublic]
 
 class OrderCreate(SQLModel):
-    customer_id: int
-    items: list[OrderItemCreate]  
-    tax_amount: Decimal = Field(max_digits=10, decimal_places=3)
-    discount_amount: Decimal = Field(max_digits=10, decimal_places=3)
-    shipping_costs_amount: Decimal = Field(max_digits=10, decimal_places=3)
+    customer_id: int = Field(nullable=False)
+    items: list[OrderItemCreate] = Field(nullable=False)
+    tax_amount: Decimal = Field(max_digits=10, decimal_places=3, ge=0, nullable=False)
+    discount_amount: Decimal = Field(max_digits=10, decimal_places=3, ge=0, nullable=False)
+    shipping_costs_amount: Decimal = Field(max_digits=10, decimal_places=3, ge=0, nullable=False)
 
     # check if there are duplicated product IDs in the list of items purchased
     # throw error if they exist
@@ -86,18 +86,29 @@ class OrderCreate(SQLModel):
        product_ids = [item.dict()['product_id'] for item in items]
 
        if len(product_ids) != len(set(product_ids)):
-        raise ValueError("Unexpected behavior detected: duplicated product IDs in one order")
+        raise ValueError("unexpected behavior detected: duplicated product IDs in one order")
 
        return items
 
-    # check if there is at lest an item to order, throw error if not
+    # check if there is at least an item to order, throw error if not
     @field_validator('items', mode='after')  
     @classmethod
     def items_to_order_exists(cls, items: list):
        product_ids = [item.dict()['product_id'] for item in items]
 
        if len(items) == 0 or items is None:
-        raise ValueError("Order should consists of at least one product to order")
+        raise ValueError("order should consists of at least one product to order")
+
+       return items
+
+    # check if there is an item with quantity of 0
+    @field_validator('items', mode='after')  
+    @classmethod
+    def items_to_order_doesnt_have_qty_of_0(cls, items: list):
+       product_quantities = [item.dict()['quantity'] for item in items]
+
+       if 0 in product_quantities:
+        raise ValueError("unexpected behavior detected: order item of quantity 0 found")
 
        return items
 
--- dummy_api/db_models.py
+++ dummy_api/db_models.py
@@ -7,29 +7,39 @@ class Product(SQLModel, table=True):
     __table_args__ = {"schema": "oltp_online_store"}
     __tablename__ = "products"
     id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(sa_type=String(50), index=False, nullable=False, unique=True)
-    current_price: Decimal = Field(nullable=False, max_digits=10, decimal_places=3)
+    name: str = Field(sa_type=String(50), 
+                      index=False, 
+                      nullable=False, 
+                      unique=True, 
+                      max_length=50, 
+                      min_length=1)
+    current_price: Decimal = Field(nullable=False, max_digits=10, decimal_places=3, ge=0)
 
 class Country(SQLModel, table=True):
     __table_args__ = {"schema": "oltp_online_store"}
     __tablename__ = "countries"
     id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(sa_type=String(50), index=False, nullable=False, unique=True)
+    name: str = Field(sa_type=String(50), 
+                      index=False, 
+                      nullable=False, 
+                      unique=True, 
+                      max_length=50, 
+                      min_length=2)
 
 class Customer(SQLModel, table=True):
     __table_args__ = {"schema": "oltp_online_store"}
     __tablename__ = "customers"
     id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(sa_type=String(50), index=False, nullable=False)
-    username: str = Field(sa_type=String(50), index=False, nullable=False, unique=True)
-    email: str = Field(sa_type=String(100), index=False, nullable=False, unique=True)
+    name: str = Field(sa_type=String(50), index=False, nullable=False, max_length=50, min_length=1)
+    username: str = Field(sa_type=String(50), index=False, nullable=False, unique=True, max_length=50, min_length=1)
+    email: str = Field(sa_type=String(100), index=False, nullable=False, unique=True, max_length=100, min_length=2)
     country_id: int = Field(nullable=False, foreign_key="oltp_online_store.countries.id")
 
 class Status(SQLModel, table=True):
     __table_args__ = {"schema": "oltp_online_store"}
     __tablename__ = "status"
     id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(sa_type=String(50), index=False, nullable=False, unique=True)
+    name: str = Field(sa_type=String(50), index=False, nullable=False, unique=True, max_length=50, min_length=1)
 
 class Order(SQLModel, table=True):
     __table_args__ = {"schema": "oltp_online_store"}
@@ -48,9 +58,9 @@ class Order(SQLModel, table=True):
     ))
     current_status_id: int = Field(nullable=False, foreign_key="oltp_online_store.status.id")
     customer_id: int = Field(nullable=False, foreign_key="oltp_online_store.customers.id")
-    tax_amount: Decimal = Field(nullable=False, max_digits=10, decimal_places=3)
-    discount_amount: Decimal = Field(nullable=False, max_digits=10, decimal_places=3)
-    shipping_costs_amount: Decimal = Field(nullable=False, max_digits=10, decimal_places=3)
+    tax_amount: Decimal = Field(nullable=False, max_digits=10, decimal_places=3, ge=0)
+    discount_amount: Decimal = Field(nullable=False, max_digits=10, decimal_places=3, ge=0)
+    shipping_costs_amount: Decimal = Field(nullable=False, max_digits=10, decimal_places=3, ge=0)
 
 class StatusHistory(SQLModel, table=True):
     __table_args__ = {"schema": "oltp_online_store"}
@@ -70,5 +80,5 @@ class OrderItem(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     product_id: int = Field(nullable=False, foreign_key="oltp_online_store.products.id")
     order_id: int = Field(nullable=False, foreign_key="oltp_online_store.orders.id")
-    quantity: int = Field(nullable=False)
-    price_per_unit_at_purchase: Decimal = Field(nullable=False, max_digits=10, decimal_places=3)
+    quantity: int = Field(nullable=False, ge=1)
+    price_per_unit_at_purchase: Decimal = Field(nullable=False, max_digits=10, decimal_places=3, ge=0)
