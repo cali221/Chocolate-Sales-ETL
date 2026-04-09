45790bd5b1ffe7e6be2c057f83987da2e3e4276f
cali221 <theresiacalista57@gmail.com>
2026-04-10 00:27:36 +0700

    made time.sleep after random order creation and after update on random orders' status shorter

--- online_store_simulator/main.py
+++ online_store_simulator/main.py
@@ -21,8 +21,8 @@ while True:
     # simulate random events in the online store with pauses:
     # create one random order
     create_random_order(host, port)
-    time.sleep(random.randint(2, 50))
+    time.sleep(random.randint(2, 5))
 
     # update one or more orders' status
     update_random_orders_status(host, port)
-    time.sleep(random.randint(30, 120))
+    time.sleep(random.randint(2, 5))
