1cff781102b75b5b67bffa57840650903938fed2
Theresia Calista <theresiacalista57@gmail.com>
2026-04-12 16:07:39 +0700

    progress on fct_online_store_ordered_items (now using allocated monetary values

--- transform/choco_sales/models/intermediate/orders/int_orders_with_names_and_totals.sql
+++ transform/choco_sales/models/intermediate/orders/int_orders_with_names_and_totals.sql
@@ -9,20 +9,21 @@ GROUP BY order_item_order_id)
 
 -- get relevant data from orders
 SELECT o.order_id AS order_id,
-       o.order_customer_id AS customer_id,
-       c.customer_country_id AS customer_country_id,
-       o.order_created_at AS created_at,
-       o.order_status_last_updated_at AS status_last_updated_at,
-       o.order_current_status_id AS current_status_id,
+       o.order_customer_id AS order_customer_id,
+       c.country_name AS customer_country_name,
+       o.order_created_at,
+       o.order_status_last_updated_at,
+       s.status_name AS current_status_name,
+       o.order_tax_amount_usd,
+       o.order_discount_amount_usd,
+       o.order_shipping_costs_amount_usd,
        oi.number_of_distinct_products AS number_of_distinct_products,
        oi.total_items_ordered AS total_items_ordered,
-       oi.subtotal AS subtotal_usd,
-       CAST((oi.subtotal + 
-             o.order_shipping_costs_amount_usd + 
-             o.order_tax_amount_usd - 
-             o.order_discount_amount_usd) AS NUMERIC(10, 3)) AS total_amount_usd
+       oi.subtotal AS subtotal_usd
 FROM order_item_data oi 
 JOIN {{ref('stg_online_store__orders')}} o
 ON o.order_id = oi.order_id
-JOIN {{ref('stg_online_store__customers')}} c
+JOIN {{ref('int_customers_with_country')}} c
 ON o.order_customer_id = c.customer_id
+JOIN {{ref('stg_online_store__statuses')}} s
+ON s.status_id = o.order_current_status_id
--- transform/choco_sales/models/marts/fct_online_store_ordered_items.sql
+++ transform/choco_sales/models/marts/fct_online_store_ordered_items.sql
@@ -1,5 +1,5 @@
 -- TODO: https://greatobi.wordpress.com/2009/07/16/modeling-header-details/ 
--- use allocated numerical values instead
+-- instead of allocated val = val/# of distinct product other business rules can be applied
 SELECT oi.order_item_id,
        oi.order_item_order_id,
        {{ dbt_utils.generate_surrogate_key(['oi.order_item_product_name']) }} AS product_id,
@@ -9,12 +9,10 @@ SELECT oi.order_item_id,
        o.order_status_last_updated_at,
        o.current_status_name,
        {{ dbt_utils.generate_surrogate_key(['o.order_customer_id']) }} AS customer_id,
-       {{ dbt_utils.generate_surrogate_key(['c.country_name'])}} AS customer_country_id,
-       o.order_tax_amount_usd,
-       o.order_discount_amount_usd,
-       o.order_shipping_costs_amount_usd
+       {{ dbt_utils.generate_surrogate_key(['o.customer_country_name'])}} AS customer_country_id,
+       o.order_tax_amount_usd/o.number_of_distinct_products AS allocated_tax,
+       o.order_discount_amount_usd/o.number_of_distinct_products AS allocated_discount,
+       o.order_shipping_costs_amount_usd/o.number_of_distinct_products AS allocated_shipping_costs
 FROM {{ref('int_order_items_with_product_name')}} oi
-JOIN {{ref('int_orders_with_status_name')}} o
+JOIN {{ref('int_orders_with_names_and_totals')}} o
 ON o.order_id = oi.order_item_order_id
-JOIN {{ref('int_customers_with_country')}} c 
-ON o.order_customer_id = c.customer_id
